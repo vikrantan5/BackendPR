872f024faf58fe44da0485ad1fdf73d08d04ffc0
Vikrant Singh <vikrantsinghan5@gmail.com>
2026-09-19 18:05:01 +0530

    auth

--- src/app.js
+++ src/app.js
@@ -1,3 +1,57 @@
+const express = require("express");
+
+const app = express();
+const {adminAuth, userAuth} =require("./middlewares/auth")
+
+
+app.use("/user" ,userAuth )
+
+app.get("/user/editProfiledata" ,(req , res)=>{
+    res.send("user")
+} )
+
+
+app.get("/admin/deleteDate" , adminAuth,(req , res)=>{
+    res.send("deleting th user")
+})
+
+app.listen(3000, () => {
+  console.log("hehe chacha");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get(
+//   "/user",
+//   (req, res , next) => {
+//     // route handler
+//     // res.send("rout handler 1");
+//     //kuch send nhi karenge  to atak jayega
+//     //after time out nothing will return
+//     // we have to send the response back
+//     next()
+//     res.send("chcha");
+//     console.log("pahla")
+
+//     console.log("dusra")
+//   },
+//   (req, res) => {
+//     // res.send("handling route from route handler 2");
+//   },
+// );
+
 // console.log("staring the project")
 
 // const express = require("express")
@@ -9,28 +63,25 @@
 
 // })
 
-
 // app.listen(3000 , ()=>{
 //     console.log("server is runnning on the 3000")
 // })
-
-
-
-const express = require("express")
-
-const app = express()
-
-
-app.use("/sowhat" , (req ,res)=>{
-    res.send("sowhat")
-})
-app.use("/" ,(req ,res)=>{
-    res.send("lala chacha")
-})
-
-
-
-app.listen(3000 , ()=>{
-    console.log("hehe chacha")
-})
-
+// app.get(
+//   "/user",
+// [  (req, res, next) => {
+//     // res.send("first route handler");
+//     next();
+//   },
+//   (req, res, next) => {
+//     // res.send("sec route handler");
+//     next();
+//   }],
+//   (req, res, next) => {
+//     // res.send("thir route handler");
+//     next();
+//   },
+//   (req, res, next) => {
+//     res.send("fourth route handler");
+//     // next();
+//   }
+// );
---
+++ src/middlewares/auth.js
@@ -0,0 +1,28 @@
+
+
+ const userAuth = (req , res , next)=>{
+    const isToken = "vikrant";
+    const isAuth = isToken==="vikrvvant"
+    if(isAuth){
+        next()
+    }
+    else{
+        res.status(401).send("password not match")
+    }
+}
+
+ const adminAuth = (req , res , next)=>{
+    const isToken = "vikrant";
+    const isAuth = isToken==="vikrangft"
+    if(isAuth){
+        next()
+    }
+    else{
+        res.status(401).send("password not match")
+    }
+}
+
+
+module.exports = {
+    adminAuth , userAuth
+}
